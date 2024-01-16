a86cb029de3417fdb46d59e6ba1b69aa933d5dff
PowerViber <145151999+PowerViber@users.noreply.github.com>
2024-01-16 20:22:41 +0700

    Update tugas1.py

--- tugas/tugas1/tugas1.py
+++ tugas/tugas1/tugas1.py
@@ -1,4 +1,26 @@
 import cv2
 import numpy as np
 
-# Tulis Kodingan kalian dibawah
+image = cv2.imread("MagangBayu2024-OpenCV/tugas/tugas1/tugas1.png")
+
+image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+lower = np.array([75, 20, 20])
+upper = np.array([100, 255, 255])
+
+mask = cv2.inRange(image_hsv, lower, upper)
+
+contours, _= cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+if len(contours) != 0:
+    for contour in contours:
+        if cv2.contourArea(contour) > 300:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(image, (x,y), (x+w, y+h), (100, 255, 100), 2)
+
+cv2.imshow("Mask", mask)
+cv2.imshow("Gambar", image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
